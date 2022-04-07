9af0987b550505724beb5548d6eb6aedfb4bb3dc
오경환 <okh01@interpark.com>
2022-04-07 10:16:49 +0900

    멤버십 등록 성공 테스트

--- src/test/java/com/study/tddmembership/membership/controller/MembershipController.java
+++ src/test/java/com/study/tddmembership/membership/controller/MembershipController.java
@@ -23,9 +23,10 @@ public class MembershipController {
       @RequestHeader(USER_ID_HEADER) final String userId,
       @RequestBody @Valid final MembershipRequest membershipRequest) {
 
-    membershipService.addMembership(
-        userId, membershipRequest.getMembershipType(), membershipRequest.getPoint());
+    final MembershipResponse membershipResponse =
+        membershipService.addMembership(
+            userId, membershipRequest.getMembershipType(), membershipRequest.getPoint());
 
-    return ResponseEntity.status(HttpStatus.CREATED).build();
+    return ResponseEntity.status(HttpStatus.CREATED).body(membershipResponse);
   }
 }
--- src/test/java/com/study/tddmembership/membership/controller/MembershipControllerTest.java
+++ src/test/java/com/study/tddmembership/membership/controller/MembershipControllerTest.java
@@ -4,14 +4,18 @@ import com.google.gson.Gson;
 import static com.study.tddmembership.membership.controller.MembershipConstants.USER_ID_HEADER;
 import com.study.tddmembership.membership.exception.MembershipErrorResult;
 import com.study.tddmembership.membership.exception.MembershipException;
+import com.study.tddmembership.membership.response.MembershipResponse;
 import com.study.tddmembership.membership.service.MembershipService;
 import com.study.tddmembership.membership.type.MembershipType;
+import java.nio.charset.StandardCharsets;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
@@ -39,6 +43,35 @@ class MembershipControllerTest {
         MockMvcBuilders.standaloneSetup(target)
             .setControllerAdvice(new GlobalExceptionHandler())
             .build();
+  }
+
+  @Test
+  @DisplayName("멤버십등록성공")
+  void membershipAddSuccess() throws Exception{
+    // given
+    final String url = "/api/v1/memberships";
+    final MembershipResponse membershipResponse = MembershipResponse.builder()
+        .id(-1L)
+        .membershipType(MembershipType.NAVER).build();
+    doReturn(membershipResponse).when(membershipService).addMembership("12345", MembershipType.NAVER, 10000);
+
+    // when
+    final ResultActions resultActions = mockMvc.perform(
+        MockMvcRequestBuilders.post(url)
+            .header(USER_ID_HEADER,"12345")
+            .content(gson.toJson(membershipRequest(10000, MembershipType.NAVER)))
+            .contentType(MediaType.APPLICATION_JSON)
+    );
+
+    // then
+    resultActions.andExpect(status().isCreated());
+
+    final MembershipResponse response = gson.fromJson(resultActions.andReturn()
+        .getResponse()
+        .getContentAsString(StandardCharsets.UTF_8), MembershipResponse.class);
+
+    assertThat(response.getMembershipType()).isEqualTo(MembershipType.NAVER);
+    assertThat(response.getId()).isNotNull();
   }
 
   @Test
